7b69cc3d11a3b3da9d611631be2806e6aeb70e14
veselinovStf <chofexx@gmail.com>
2020-01-03 22:47:40 +0200

    project base structure is done

--- ng-front-end/src/app/app.component.ts
+++ ng-front-end/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Helpers } from './helpers/helpers';
+import { startWith, delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-front-end';
+export class AppComponent implements AfterViewInit {
+
+  subscription: Subscription;
+
+  authentication: boolean;
+
+  constructor(private helpers: Helpers) {
+
+  }
+
+  ngAfterViewInit() {
+
+    this.subscription = this.helpers.isAuthenticationChanged().pipe(
+      startWith(this.helpers.isAuthenticated()),
+      delay(0)).subscribe((value) =>
+        this.authentication = value
+      );
+
+  }
+
+  title = 'Angular 5 Seed';
+
+  ngOnDestroy() {
+
+    this.subscription.unsubscribe();
+
+  }
+
 }
