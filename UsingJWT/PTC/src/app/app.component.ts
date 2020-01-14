095cdd10d04d13eb656509bf6c018c07b66c8fb3
veselinovStf <chofexx@gmail.com>
2020-01-14 09:39:24 +0200

    adding ng structure

--- UsingJWT/PTC/src/app/app.component.ts
+++ UsingJWT/PTC/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'ptc-root',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'PTC';
+  title: string = "Paul's Training Company";
 }
--- UsingJWT/PTC/src/app/app.module.ts
+++ UsingJWT/PTC/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product/product-list.component';
+import { CategoryListComponent } from './category/category-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductDetailComponent } from './product/product-detail.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { ProductService } from './product/product.service';
+import { CategoryService } from './category/category.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    CategoryListComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [ProductService, CategoryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
